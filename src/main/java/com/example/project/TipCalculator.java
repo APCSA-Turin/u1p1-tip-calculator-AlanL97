13b882de04b6bad161768b6eeffa4669586b46ab
AlanL97 <alanl97@nycstudents.net>
2024-09-19 16:27:02 +0000

    second commit

--- src/main/java/com/example/project/TipCalculator.java
+++ src/main/java/com/example/project/TipCalculator.java
@@ -6,15 +6,20 @@ public class TipCalculator {
         //DO NOT DELETE ANY OF THE CODE BELOW      
         StringBuilder result = new StringBuilder();
         //your code here
+        double totalTip = (int) (percent/100.0*cost*100+0.5)/100.0;
+        double totalBill = (int) ((cost+totalTip)*100+0.5)/100.0;
+        double perPersonBeforeTip = (int) (cost/people*100+0.5)/100.0;
+        double tipPerPerson = (int) (totalTip/people*100+0.5)/100.0;
+        double totalCostPerPerson = (int) (totalBill/people*100+0.5)/100.0;
                        
         result.append("-------------------------------\n");
-        result.append("Total bill before tip: "); //concatenate to this string. DO NOT ERASE AND REWRITE
-        result.append("Total percentage: ");
-        result.append("Total tip: ");
-        result.append("Total Bill with tip: ");
-        result.append("Per person cost before tip: ");
-        result.append("Tip per person: ");
-        result.append("Total cost per person: ");
+        result.append("Total bill before tip: " + "$" + cost + "\n"); //concatenate to this string. DO NOT ERASE AND REWRITE
+        result.append("Total percentage: " + percent + "%\n");
+        result.append("Total tip: " + "$" + totalTip + "\n");
+        result.append("Total Bill with tip: " + "$" + totalBill + "\n");
+        result.append("Per person cost before tip: " + "$" + perPersonBeforeTip + "\n");
+        result.append("Tip per person: " + "$" + tipPerPerson + "\n");
+        result.append("Total cost per person: " + "$" + totalCostPerPerson + "\n");
         result.append("-------------------------------\n");
 
 
@@ -23,9 +28,9 @@ public class TipCalculator {
      //TEST YOUR PROGRAM IN main
      public static void main(String[] args) {
         //try different values for people, percent, and cost to test your program before running test cases
-        int people; 
-        int percent;
-        double cost;              
+        int people = 12; 
+        int percent = 15;
+        double cost =566.97;              
         System.out.println(calculateTip(people,percent,cost));
     }
 }
